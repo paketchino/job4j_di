431dd438642ea78ccad40e0b3ae28a54fa79810b
paketchino <chansgorn@gmail.com>
2022-09-08 23:02:01 +0300

    1. Spring DI. Запуск [#2091]

--- src/main/java/jobj/Job4jDiApplication.java
+++ src/main/java/jobj/Job4jDiApplication.java
@@ -1,13 +1,28 @@
 package jobj;
 
+import jobj.di.store.Store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
 public class Job4jDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Job4jDiApplication.class, args);
+		StartUI ui;
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
+			context.register(StartUI.class);
+			context.register(ConsoleInput.class);
+			context.register(Store.class);
+
+			context.refresh();
+
+			ui = context.getBean(StartUI.class);
+		}
+		String question = "question";
+		ui.add("Petr Arsentev");
+		ui.add("Ivan ivanov");
+		ui.print();
+		ui.askStr(question);
 	}
 
 }
